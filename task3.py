6520faef835e48aa458389275b63127b5940f179
Barrakuda <brpslelush@gmail.com>
2021-05-16 13:49:36 +0200

    Final commit

--- task3.py
+++ task3.py
@@ -12,35 +12,36 @@ deque – это обобщение стеков и очередей.
 
 # Заполнение списка и очереди происходит с одинаковой скорость, получение последнего элемента тоже.
 # Заполнение очереди слева выполняется во много раз быстрее заполнения слева списка.
+# Точно так же как и получение элемента слева и extend слева.
 # Что по сути весьма очевидно, потому что очередь для этого и нужна.
 
 from collections import deque
 from timeit import timeit
 
 
-def lst_fulfill():
+def lst_fulfill(num):
     lst = []
-    for i in range(100000):
+    for i in range(num):
         lst.append(i)
     return lst
 
 
-def deq_fulfill():
+def deq_fulfill(num):
     deq = deque()
-    for i in range(100000):
+    for i in range(num):
         deq.append(i)
     return deq
 
 
-def lst_fulfill_left():
+def lst_fulfill_left(num):
     lst = []
-    for i in range(10000):
+    for i in range(num):
         lst.insert(0, i)
 
 
-def deq_fulfill_left():
+def deq_fulfill_left(num):
     deq = deque()
-    for i in range(10000):
+    for i in range(num):
         deq.appendleft(i)
 
 
@@ -52,11 +53,48 @@ def deq_pop(deq):
     return deq.pop()
 
 
-lst_a = lst_fulfill()
-deq_a = deq_fulfill()
-print(timeit('lst_fulfill()', number=10000, globals=globals()))
-print(timeit('deq_fulfill()', number=10000, globals=globals()))
-print(timeit('lst_fulfill_left()', number=10000, globals=globals()))
-print(timeit('deq_fulfill_left()', number=10000, globals=globals()))
-print(timeit('lst_pop(lst_a)', number=100000, globals=globals()))
-print(timeit('deq_pop(deq_a)', number=100000, globals=globals()))
+def lst_pop_left(lst):
+    return lst.pop(0)
+
+
+def deq_pop_left(deq):
+    return deq.popleft()
+
+
+def lst_extend_left(lst, lst2):
+    lst = lst2 + lst
+    return lst
+
+
+def deq_extend_left(deq, deq2):
+    deq.extend(deq2)
+    return deq
+
+
+print('Append')
+print(timeit('lst_fulfill(10000)', number=10000, globals=globals()))
+print(timeit('deq_fulfill(10000)', number=10000, globals=globals()))
+
+print('Append left')
+print(timeit('lst_fulfill_left(10000)', number=10000, globals=globals()))
+print(timeit('deq_fulfill_left(10000)', number=10000, globals=globals()))
+
+print('pop')
+lst_a = lst_fulfill(30000)
+deq_a = deq_fulfill(30000)
+print(timeit('lst_pop(lst_a)', number=10000, globals=globals()))
+print(timeit('deq_pop(deq_a)', number=10000, globals=globals()))
+
+print('pop left')
+lst_b = lst_fulfill(30000)
+deq_b = deq_fulfill(30000)
+print(timeit('lst_pop_left(lst_b)', number=10000, globals=globals()))
+print(timeit('deq_pop_left(deq_b)', number=10000, globals=globals()))
+
+print('extend left')
+lst_c = lst_fulfill(30000)
+deq_c = deq_fulfill(30000)
+lst_d = lst_fulfill(100)
+deq_d = deq_fulfill(100)
+print(timeit('lst_extend_left(lst_c, lst_d)', number=1000, globals=globals()))
+print(timeit('deq_extend_left(deq_c, deq_d)', number=1000, globals=globals()))

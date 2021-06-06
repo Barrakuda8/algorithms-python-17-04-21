f44d0f7a0d2ca5e15dfc23e2ddb7515bc5f2b9ea
Barrakuda <brpslelush@gmail.com>
2021-06-06 19:09:56 +0200

    first commit

--- task2.py
+++ task2.py
@@ -7,6 +7,11 @@
 Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
 """
 
+# Добавлена валидация значений левого и правого потомка для корня дерева. Я ещё думал как бы сделать валидацию для
+# всех последующих потомков, но для этого надо переписать половину скрипта, если не весь, и к тому же потом я посмотрел
+# пример и в нём этого нет, так что оставил валидацию только для правого и левого потомков корня.
+
+
 class BinaryTree:
     def __init__(self, root_obj):
         # корень
@@ -18,59 +23,74 @@ class BinaryTree:
 
     # добавить левого потомка
     def insert_left(self, new_node):
-        # если у узла нет левого потомка
-        if self.left_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.left_child = BinaryTree(new_node)
-        # если у узла есть левый потомок
+        if new_node < self.get_root_val():
+            # если у узла нет левого потомка
+            if self.left_child is None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.left_child = BinaryTree(new_node)
+            # если у узла есть левый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.left_child = self.left_child
+                self.left_child = tree_obj
         else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.left_child = self.left_child
-            self.left_child = tree_obj
+            print("Impossible. Too big")
 
     # добавить правого потомка
     def insert_right(self, new_node):
-        # если у узла нет правого потомка
-        if self.right_child == None:
-            # тогда узел просто вставляется в дерево
-            # формируется новое поддерево
-            self.right_child = BinaryTree(new_node)
-        # если у узла есть правый потомок
+        if new_node > self.get_root_val():
+            # если у узла нет правого потомка
+            if self.right_child is None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.right_child = BinaryTree(new_node)
+            # если у узла есть правый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.right_child = self.right_child
+                self.right_child = tree_obj
         else:
-            # тогда вставляем новый узел
-            tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
-            tree_obj.right_child = self.right_child
-            self.right_child = tree_obj
+            print("Impossible. Too small")
 
     # метод доступа к правому потомку
     def get_right_child(self):
-        return self.right_child
+        if self.right_child:
+            return self.right_child
+        else:
+            print('Нет правого потомка')
 
     # метод доступа к левому потомку
     def get_left_child(self):
-        return self.left_child
+        if self.left_child:
+            return self.left_child
+        else:
+            print('Нет левого потомка')
 
     # метод установки корня
     def set_root_val(self, obj):
-        self.root = obj
+        if self.left_child.root < obj < self.right_child.root:
+            self.root = obj
+        else:
+            print('Impossible value')
 
     # метод доступа к корню
     def get_root_val(self):
-        return self.root
+        if self.root:
+            return self.root
+        else:
+            print('Нет корня')
 
 
 r = BinaryTree(8)
-print(r.get_root_val())
-print(r.get_left_child())
-r.insert_left(40)
-print(r.get_left_child())
-print(r.get_left_child().get_root_val())
+r.insert_left(12)
+r.insert_left(4)
+r.insert_right(2)
 r.insert_right(12)
-print(r.get_right_child())
-print(r.get_right_child().get_root_val())
-r.get_right_child().set_root_val(16)
-print(r.get_right_child().get_root_val())
+print(r.get_root_val())
+print(r.left_child.get_left_child())
+print(r.left_child.get_right_child())
